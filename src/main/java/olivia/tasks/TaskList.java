b452b769d65b71f18eb562353added2c9beca601
xplus2g4 <xplus2g4@gmail.com>
2025-01-29 17:23:28 +0800

    Add find command

--- src/main/java/olivia/tasks/TaskList.java
+++ src/main/java/olivia/tasks/TaskList.java
@@ -3,30 +3,75 @@ package olivia.tasks;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList {
     private List<Task> tasks;
 
+    /**
+     * Constructs a task list.
+     *
+     * @param tasks The list of tasks.
+     */
     public TaskList(List<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the list.
+     *
+     * @param task The task to add.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes the task at the specified index.
+     *
+     * @param index The index of the task to remove.
+     */
     public void removeTask(int index) {
         tasks.remove(index);
     }
 
+    /**
+     * Returns the task at the specified index.
+     *
+     * @param index The index of the task.
+     * @return The task at the specified index.
+     */
     public Task getTask(int index) {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The number of tasks in the list.
+     */
     public int getSize() {
         return tasks.size();
     }
 
+    /**
+     * Returns a list of tasks in CSV format.
+     *
+     * @return The list of tasks in CSV format.
+     */
     public List<String> getTasksCsv() {
         return tasks.stream().map(Task::toCsvString).collect(Collectors.toList());
+    }
+
+    /**
+     * Finds tasks that contain the keyword.
+     *
+     * @param keyword The keyword to search for.
+     * @return The list of tasks that contain the keyword.
+     */
+    public TaskList findTasks(String keyword) {
+        return new TaskList(tasks.stream().filter(task -> task.getDescription().contains(keyword))
+                .collect(Collectors.toList()));
     }
 }
